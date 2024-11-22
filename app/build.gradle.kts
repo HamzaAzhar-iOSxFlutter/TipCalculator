4f68e9d2cdc993793f61dc643b0b1434b1264e71
Muhammad Hamza Azhar <hamzaazharcs@gmail.com>
2024-11-21 20:42:33 -0500

    compile SDK updated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.tipcalculator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tipcalculator"
